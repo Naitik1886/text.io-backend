16150951129dfca3f0ad4332f291d91dd0936fed
Naitik <mymailofficial1886@gmail.com>
2025-08-04 01:31:12 +0530

    feat: stabilize socket.io configuration

--- src/libs/socket.js
+++ src/libs/socket.js
@@ -5,9 +5,13 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 
+// In your src/libs/socket.js file
+
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+    credentials: true, // Allows cookies to be sent
   },
 });
 
